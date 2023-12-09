ec4ec41aa274bb1b454f9a1a60a892d1ef02ca87
ebcoleman <140854597+ebcoleman@users.noreply.github.com>
2023-12-09 11:53:46 -0500

    cleaned up old comments and structured README, still needs screenshot and link

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,10 @@
-// Assignment Code
+
 var generateBtn = document.querySelector("#generate");
 var uppercase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var numbers = [1,2,3,4,5,6,7,8,9,0];
 var symbols = ["!","@","#","$","&","*"];
+
 
 function generatePassword() {
   console.log ("button was clicked");
@@ -23,7 +24,6 @@ function generatePassword() {
   var includeNumbers = confirm("Include numbers?");
   var includeSymbols = confirm("Include symbols?");
 
-  
   
   var allCharacters = [];
 
@@ -48,10 +48,8 @@ function generatePassword() {
   }
 
 // prior to putting in the return statement this function was undefined so when you clicked the button it read "undefined" in the generator
-// once code  
   return createdPassword
-  
-}
+  }
 
 // Write password to the #password input
 function writePassword() {
@@ -59,22 +57,8 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
-// ```
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
-// ```
